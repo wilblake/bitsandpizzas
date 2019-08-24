2dbbc83fddc9c501ae06b36df5c3a64cd74a65cc
wil <wblake@emsys.net>
2019-08-23 20:26:56 -0400

    Ch 12 Design Library : Collapsing Title Bar, Coordinator Layout, Nested Scroll View, FAB, Snackbar

--- app/src/main/java/com/example/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/example/bitsandpizzas/OrderActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.support.design.widget.Snackbar;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
     @Override
@@ -15,4 +18,18 @@ public class OrderActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
+    public void onClickDone(View view) {
+        CharSequence text = "Your order has been updated";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        snackbar.show();
+    }
+
 }
